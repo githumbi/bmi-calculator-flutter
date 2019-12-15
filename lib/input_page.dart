b8b3f7594deac99e681e3e83167ddfd0d2eade9e
githumbi <githumbi74@gmail.com>
2019-12-15 10:19:46 +0300

    added container class

---
+++ lib/input_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: Color(0XFF101427),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: Color(0XFF101427),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(
+              colour: Color(0XFF101427),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: Color(0XFF101427),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: Color(0XFF101427),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.colour});
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10), color: Color(0XFF101427)),
+    );
+  }
+}
