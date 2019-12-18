55f48652a9101a2381e282fda475b8897968cf77
githumbi <githumbi74@gmail.com>
2019-12-18 16:06:20 +0300

    added constants file

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const kBottomHeightContainer = 80.0;
+const kInactiveCardColor = Color(0XFF141A3B);
+const kActiveCardColor = Color(0XFF282C4E);
+const kColorBottomButton = Color(0XFF141A3B);
+
+const kLabelTextStyle = TextStyle(color: Color(0XFFFCFDFF), fontSize: 20);
+
+const kNumberTextStyle = TextStyle(fontSize: 60, fontWeight: FontWeight.w900);
